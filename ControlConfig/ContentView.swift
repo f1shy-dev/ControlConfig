049e6bacb0b9d561e786e25cc720604d271ab270
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-06 19:06:12 -0500

    Layout

--- ControlConfig/ContentView.swift
+++ ControlConfig/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Text("Hello, World!")
+            }
+            .navigationTitle("ControlConfig")
         }
-        .padding()
     }
 }
 
