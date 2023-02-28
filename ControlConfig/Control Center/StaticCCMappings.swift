23256bf7c6c99224565c580c459bb40c71d4b983
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-28 15:41:33 -0500

    WAY more icons.

--- ControlConfig/Control Center/StaticCCMappings.swift
+++ ControlConfig/Control Center/StaticCCMappings.swift
@@ -25,18 +25,43 @@ public struct CCMappings {
         "com.apple.control-center.DisplayModule": "Brightness",
         "com.apple.mediaremote.controlcenter.nowplaying": "Media Player",
         "com.apple.Home.ControlCenter": "Home (large)",
-        "com.apple.mediaremote.controlcenter.audio": "Volume"
-        // TODO: mediacontrolsaudio, silencecallsccwidget and others
+        "com.apple.Home.CompactControlCenter": "Home (small)",
+        "com.apple.FocusUIModule": "Focus",
+        "com.apple.control-center.OrientationLockModule": "Orientation Lock",
+        "com.apple.mediaremote.controlcenter.audio": "Volume",
+        "com.apple.replaykit.controlcenter.screencapture": "Screen Recording",
+        "com.apple.TelephonyUtilities.SilenceCallsCCWidget": "Silence Calls",
+        "com.apple.control-center.AppleTVRemoteModule": "TV Remote",
+        "com.apple.control-center.PerformanceTraceModule": "Performance Tracer",
+        "com.apple.springboard.ContinuousExposeModule": "Stage Manager",
+        // TODO: mediacontrolsaudio and others
     ]
 
     public static let bundleIDBasedSFIcons: NSDictionary = [
         "com.apple.replaykit.VideoConferenceControlCenterModule": "video",
-        "com.apple.FocusUIModule": "moon",
+        "com.apple.FocusUIModule": "moon.fill",
         "com.apple.Home.ControlCenter": "homekit",
+        "com.apple.Home.CompactControlCenter": "homekit",
         "com.apple.control-center.DisplayModule": "sun.max",
+        "com.apple.control-center.OrientationLockModule": "lock.rotation",
         "com.apple.mediaremote.controlcenter.audio": "speaker.wave.2",
         "com.apple.control-center.ConnectivityModule": "wifi",
+        "com.apple.control-center.MuteModule": "bell.slash.fill",
+        "com.apple.control-center.QuickNoteModule": "note.text",
+        "com.apple.mobilenotes.SystemPaperControlCenterModule": "note.text",
+        "com.apple.springboard.ContinuousExposeModule": "squares.leading.rectangle",
+        "com.apple.control-center.FlashlightModule": "flashlight.off.fill",
+        "com.apple.control-center.CameraModule": "camera.fill",
+        "com.apple.control-center.LowPowerModule": "battery.25",
+        "com.apple.shazamkit.controlcenter.ShazamModule": "shazam.logo.fill",
+        // for lack of a better symbol
+        "com.apple.TelephonyUtilities.SilenceCallsCCWidget": "phone.down.waves.left.and.right",
+        "com.apple.replaykit.controlcenter.screencapture": "record.circle",
         "com.apple.replaykit.AudioConferenceControlCenterModule": "mic",
+        "com.apple.control-center.AppleTVRemoteModule": "appletvremote.gen4.fill",
+        "com.apple.control-center.FeedbackAssistanceModule": "exclamationmark.bubble.fill",
+        "com.apple.control-center.PerformanceTraceModule": "waveform.path.ecg",
         "com.apple.mediaremote.controlcenter.nowplaying": "waveform"
+        // TODO: more.
     ]
 }
