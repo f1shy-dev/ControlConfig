6b9ebe3d11fc57f85148115f3db9ad39d5597193
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-04 18:03:41 -0500

    ""

--- ControlConfig/Views/CustomisationCard.swift
+++ ControlConfig/Views/CustomisationCard.swift
@@ -88,7 +88,7 @@ struct CustomisationCard: View {
         }) {
             EditModuleView(customisation: customisation, appState: appState, saveToUserDefaults: saveToUserDefaults)
         }
-        .confirmationDialog("Are you sure you want to delete the customisation \(customisation.module.description)?", isPresented: $showingDeleteConfirmation, titleVisibility: .visible) {
+        .confirmationDialog("Are you sure you want to delete the customisation \"\(customisation.module.description)\"?", isPresented: $showingDeleteConfirmation, titleVisibility: .visible) {
             Button("Delete", role: .destructive) {
                 withAnimation {
                     deleteCustomisation(customisation.self)
